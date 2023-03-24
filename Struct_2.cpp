2b04a64d7a8325924087130675843f5ddc0afb83
Esteban-can <125224817+Esteban-can@users.noreply.github.com>
2023-03-23 22:40:02 -0600

    primer_commit

---
+++ Struct_2.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+struct Estudiante{
+	int *codigo;
+	string *nombre;
+	int **nota;
+	
+};
+main()
+{
+	Estudiante estudiante;
+	int fila =0,columna=0;
+	cout<<"Cuantos Estudiantes desea Agregar:";
+	cin >>fila;
+	cout<<"Cuantos Notas por Estudiantes desea Agregar:";
+	cin >>columna;
+	estudiante.codigo = new int [fila];
+	estudiante.nombre = new string [fila];
+	estudiante.nota = new int *[fila];
+	for (int i = 0; i<fila;i++)
+	{
+		estudiante.nota[i] = new int [columna];
+	}
+	cout<<"_____________Ingreso de Notas_____________"<<endl;
+	for (int i = 0; i<fila;i++)
+	{
+		cout<<"Codigo["<<i<<"]:";
+		cin>>estudiante.codigo[i];
+		cin.ignore();
+		cout<<"Nombre Completo["<<i<<"]:";
+		getline(cin,estudiante.nombre[i]);
+		for (int ii = 0; ii<columna;ii++)
+	{
+		cout<<"Ingrese Nota["<<ii<<"] : ";
+		cin>>*(*(estudiante.nota+i)+ii);
+	}
+	cout<<"_____________________________________________________"<<endl;
+	}
+	cout<<"______________Mostrar datos___________________________"<<endl;
+		for (int i = 0; i<fila;i++)
+	{
+		cout<<"Codigo["<<i<<"]:"<<estudiante.codigo[i]<<endl;
+		cout<<"Nombre Completo["<<i<<"]:"<<estudiante.nombre[i]<<endl;
+		
+		for (int ii = 0; ii<columna;ii++)
+	{
+		cout<<"Ingrese Nota["<<ii<<"] : "<<*(*(estudiante.nota+i)+ii);
+	}
+	cout<<"_____________________________________________________"<<endl;
+	}
+	
+	for (int i = 0; i<fila;i++)
+	{
+		delete [] estudiante.nota[i] ;
+	}
+	delete []estudiante.codigo;
+    delete []	estudiante.nombre ;
+	delete []estudiante.nota;
+	
+	system("pause");
+}
